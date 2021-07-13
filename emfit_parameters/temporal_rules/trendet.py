7df632969d14f04b3491400a533e1d2ee0d4b914
Yang Gao <36057877+ebome@users.noreply.github.com>
2021-07-13 19:32:22 +0800

    Update trendet.py

--- emfit_parameters/temporal_rules/trendet.py
+++ emfit_parameters/temporal_rules/trendet.py
@@ -1,53 +1,32 @@
 # ref:
 # https://blog.csdn.net/u012111465/article/details/82017402
+# https://blog.csdn.net/u012111465/article/details/82627185
 
 
 import numpy as np
 from statsmodels.tsa.stattools import adfuller as ADF
+from scipy import stats
 
 def trend_desc(inputdata):
-    # compute the rank sum of time series
-    inputdata = np.array(inputdata)
     n = inputdata.shape[0]
-    sum_sgn = 0
-    for i in np.arange(n):
-        if i <= (n - 1):
-            for j in np.arange(i+1,n):
-                if inputdata[j] > inputdata[i]:
-                    sum_sgn = sum_sgn+1
-                elif inputdata[j] < inputdata[i]:
-                    sum_sgn = sum_sgn-1
-                else:
-                    sum_sgn = sum_sgn
-    # compute Z value
-    if n <= 10:
-        Z_value = sum_sgn/(n*(n-1)/2)
-    else:
-        if sum_sgn > 0:
-            Z_value = (sum_sgn-1)/np.sqrt(n*(n-1)*(2*n+5)/18)
-        elif sum_sgn == 0:
-            Z_value = 0
-        else:
-            Z_value = (sum_sgn+1)/np.sqrt(n*(n-1)*(2*n+5)/18)
+    if n<5: # one point cannot make sense
+        return 'S'  
+    
     # stationarity of time series: ADF test
-    ADF_result = ADF(inputdata,0)
-    # describe trends
-    # 99% ——> ±2.576
-    # 95% ——> ±1.96
-    # 90% ——> ±1.645
+    ADF_result = ADF(inputdata,0)       
     if ADF_result[1]<0.05: # p value of ADF < 0.05:  time series is stationary
-        result_desc = 'steady'
+        return 'S' # steady
+       
+        
+    # simple linear trend degree (0-1 standardized rawdata)
+    inputdataT = (np.array(inputdata)-np.min(inputdata))/(np.max(inputdata)-np.min(inputdata))
+    x = list(range(1,len(inputdata)+1))
+    xT = 1.0*(np.array(x)-np.min(x))/(np.max(x)-np.min(x))
+    clf_result = stats.linregress(xT,inputdataT)
+    linearTrendDegree = clf_result[0]
+    if linearTrendDegree > 0:
+        return 'R' # increasing
     else:
-        if np.abs(Z_value) > 1.96 and np.abs(Z_value) <= 2.576:
-            if Z_value > 0:
-                result_desc = 'increasing'
-        else:
-                result_desc = 'decresing'
-        if np.abs(Z_value) > 2.576:
-            if Z_value > 0:
-                result_desc = 'sig increasing'
-            else:
-                result_desc = 'sig decreasing' # significantly decreased
-        else:
-            result_desc     = 'unobvious trend'
-    return result_desc
+        return 'F' # decresing
+
+    
